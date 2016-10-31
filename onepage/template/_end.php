2cca6366edb8ead0df3b15ccb88bc77eb71d9393
Jascha Gerles <Jascha Gerles>
2016-10-31 11:32:55 +0100

    Pfade angepasst

--- onepage/template/_end.php
+++ onepage/template/_end.php
@@ -1,8 +1,8 @@
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="components/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="components/js/vendor/bootstrap.min.js"></script>
 
-        <script src="js/main.js"></script>
+        <script src="components/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
