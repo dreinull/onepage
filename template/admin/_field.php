468512033b4b2ff59ef7ab2fe4480d39c84a4efd
Jascha Gerles <Jascha Gerles>
2016-11-21 00:10:35 +0100

    Add dreinull/markup

--- template/admin/_field.php
+++ template/admin/_field.php
@@ -1,20 +1,30 @@
+<?php use Markup\Form; ?>
+
 <?php foreach($fields as $field) {
     echo '<div class="form-group">';
-    echo '<label for="' . $field->name . '">' . $field->placeholder . '</label>';
     switch($field->type) {
         case 'string':
-            echo '<input type="text" name="' . $field->name . '" placeholder="' . $field->placeholder . '" value="' . $data[$field->name] . '" class="form-control input-sm">';
+            echo Form::text($field->name, $field->placeholder, $data[$field->name], [
+                'class' => 'form-control input-sm'
+            ]);
             break;
         case 'text':
-            echo '<textarea name="' . $field->name . '" placeholder="' . $field->placeholder . '" class="form-control input-sm" rows="7">' . $data[$field->name] . '</textarea>';
+            echo Form::textarea($field->name, $field->placeholder, $data[$field->name], [
+                'class' => 'form-control input-sm',
+                'rows' => 8
+            ]);;
             break; // the rules!
         case 'timestamp':
-            $value = array_key_exists($field->name, $data) ? date("Y-m-d\TH:i:s", $data[$field->name]) : '';
-            echo '<input type="date" name="' . $field->name . '" placeholder="' . $field->placeholder . '" value="' . $value . '" class="form-control input-sm">';
+            $value = array_key_exists($field->name, $data) ? date("Y-m-d H:i:s", $data[$field->name]) : '';
+            echo Form::date($field->name, $field->placeholder, $value, [
+                'class' => 'form-control input-sm'
+            ]);
             break;
         case 'integer':
         case 'float':
-            echo '<input type="number" name="' . $field->name . '" placeholder="' . $field->placeholder . '" value="' . $data[$field->name] . '" class="form-control input-sm">';
+            echo Form::text($field->name, $field->placeholder, $data[$field->name], [
+                'class' => 'form-control input-sm'
+            ]);
             break;
         case 'boolean':
             $value = array_key_exists($field->name, $data) ? $data[$field->name] : 0;
